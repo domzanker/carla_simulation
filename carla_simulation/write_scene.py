5b1f5daf0fe1776e1d2a2a3d1951a7e11eb41402
Dominic Zanker <mail@dzanker.de>
2020-12-10 11:27:35 +0100

    tick one second before starting simulation

--- carla_simulation/write_scene.py
+++ carla_simulation/write_scene.py
@@ -89,7 +89,7 @@ def write_scene(args, client=None, world=None):
     spectator.set_transform(spec)
     step = 0
 
-    [world.tick() for _ in range(10)]
+    [world.tick() for _ in range(ticks_per_scene)]
 
     with trange(ticks_per_scene, leave=False, smoothing=0) as t_range:
         for i in t_range:
